45ed3f67b4466ad81b0e6a6614e7b4750a104b23
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-05-09 01:37:27 +0530

    migrated to MERN

--- sauth/src/app/_components/DashBoard.tsx
+++
@@ -1,3 +0,0 @@
-export default async function DashBoard() {
-    return <span>dashboard</span>;
-}
--- sauth/src/app/_components/Hero.tsx
+++
@@ -1,8 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-import { SignIn, SignedIn } from '../lib/util/main';
-
-export default async function Hero() {
-    // const isSignedIn = await SignedIn();
-    // return <>{isSignedIn ? <SignIn /> : <span>Signed in</span>}</>;
-    return <SignUp path="/sign-up" />;
-}
